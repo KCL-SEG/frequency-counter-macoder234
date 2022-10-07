9f0c0f803068f29ad7c6b81b4d9ad5ac26cc369a
Manik Aggarwal <manik.aggarwal@kcl.ac.uk>
2022-10-07 14:27:31 +0100

    test

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,31 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 def frequencies(items):
+
     frequencies = {}
-    # Your code goes here
+
+    newList = []
+    for x in items:
+        if isinstance(x, str):
+            newList.append(x)
+        else:
+            newList.append(f"{x}")
+
+    for x in newList:
+        frequencies.update({x: newList.count(x)})
+
+    print(frequencies)
+
     return frequencies
+
+
+# while True:
+#     try:
+#         items = [input().split(",")]
+#         frequencies(items)
+#     except ValueError:
+#         print("Some input could not be converted to a number!")
+#     else:
+#         break
